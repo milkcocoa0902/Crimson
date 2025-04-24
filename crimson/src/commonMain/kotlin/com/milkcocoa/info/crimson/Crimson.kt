b3d089fa4ab9fa45491c146e1bbdc7154d60caef
keita <developer@milkcocoa.info>
2025-04-25 00:23:04 +0900

    :thread: frameを至るところでconsumeAsFlowしていたのを、SharedFlowを構築するように

--- crimson/src/commonMain/kotlin/com/milkcocoa/info/crimson/Crimson.kt
+++ crimson/src/commonMain/kotlin/com/milkcocoa/info/crimson/Crimson.kt
@@ -3,7 +3,6 @@ package com.milkcocoa.info.crimson
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.websocket.DefaultClientWebSocketSession
 import io.ktor.client.plugins.websocket.webSocketSession
-import io.ktor.http.headersOf
 import io.ktor.utils.io.core.toByteArray
 import io.ktor.websocket.CloseReason
 import io.ktor.websocket.Frame
@@ -19,6 +18,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.consumeAsFlow
@@ -27,6 +27,7 @@ import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
@@ -47,9 +48,11 @@ class Crimson<SND: CrimsonData, RCV: CrimsonData>(
     private val _connectionStatus = MutableStateFlow(ConnectionState.CLOSED)
     val connectionStatus: StateFlow<ConnectionState> = _connectionStatus
 
-    private var session: DefaultClientWebSocketSession? = null
-    private val _incomingFlow = MutableSharedFlow<RCV>()
-    val incoming: SharedFlow<RCV> get() = _incomingFlow
+    private var webSocketSession: DefaultClientWebSocketSession? = null
+    private var incomingFrameFlow: SharedFlow<Frame>? = null
+
+    private val _incomingMessageFlow = MutableSharedFlow<RCV>()
+    val incomingMessage: SharedFlow<RCV> get() = _incomingMessageFlow
 
     private val mutex = Mutex()
 
@@ -75,11 +78,12 @@ class Crimson<SND: CrimsonData, RCV: CrimsonData>(
 
     override suspend fun execute(command: CrimsonCommand) {
         when(command){
-            is CrimsonCommand.Ping -> session?.send(Frame.Ping(command.frame))
-            is CrimsonCommand.Pong -> session?.send(Frame.Pong(command.frame))
+            is CrimsonCommand.Ping -> webSocketSession?.send(Frame.Ping(command.frame))
+            is CrimsonCommand.Pong -> webSocketSession?.send(Frame.Pong(command.frame))
             is CrimsonCommand.StartHealthCheck -> {
-                this.session ?: return
-                launchHealthCheck(this@Crimson.session!!)
+                this.webSocketSession ?: return
+                this.incomingFrameFlow ?: return
+                launchHealthCheck(this@Crimson.incomingFrameFlow!!)
             }
             is CrimsonCommand.Connect -> {
                 mutex.withLock {
@@ -94,15 +98,23 @@ class Crimson<SND: CrimsonData, RCV: CrimsonData>(
                         }
                     }.onSuccess {
                         _connectionStatus.value = ConnectionState.CONNECTED
-                        this@Crimson.session = it
+                        this@Crimson.webSocketSession = it
 
-                        handleIncomingFrame(this@Crimson.session!!)
-                        respondToPingFrame(this@Crimson.session!!)
-                        closeMonitor(this@Crimson.session!!)
+                        this@Crimson.incomingFrameFlow = it.incoming
+                            .consumeAsFlow()
+                            .shareIn(
+                                scope = coroutineScope,
+                                started = SharingStarted.Eagerly,
+                                replay = 0
+                            )
+
+                        handleIncomingFrame(this@Crimson.incomingFrameFlow!!)
+                        respondToPingFrame(this@Crimson.incomingFrameFlow!!)
+                        closeMonitor(this@Crimson.webSocketSession!!)
 
                         crimsonHandler?.onConnect(
                             crimson = this@Crimson,
-                            flow = _incomingFlow
+                            flow = _incomingMessageFlow
                         )
                     }.onFailure {
                         _connectionStatus.value = ConnectionState.CLOSED
@@ -113,8 +125,9 @@ class Crimson<SND: CrimsonData, RCV: CrimsonData>(
             is CrimsonCommand.Disconnect -> {
                 mutex.withLock {
                     _connectionStatus.value = ConnectionState.CLOSED
-                    session?.close(reason = CloseReason(code = command.code.toShort(), message = command.reason))
-                    session = null
+                    webSocketSession?.close(reason = CloseReason(code = command.code.toShort(), message = command.reason))
+                    webSocketSession = null
+                    incomingFrameFlow = null
                     if(command.abnormally){
                         resetScope()
                         coroutineScope.launch {
@@ -130,11 +143,11 @@ class Crimson<SND: CrimsonData, RCV: CrimsonData>(
         if(_connectionStatus.value != ConnectionState.CONNECTED){
             throw IllegalStateException()
         }
-        if(session == null){
+        if(webSocketSession == null){
             throw IllegalStateException()
         }
 
-        session!!.send(frame = Frame.Text(text = json.encodeToString(outgoingSerializer, data)))
+        webSocketSession!!.send(frame = Frame.Text(text = json.encodeToString(outgoingSerializer, data)))
     }
 
     private var retryingJob: Job? = null
@@ -169,11 +182,10 @@ class Crimson<SND: CrimsonData, RCV: CrimsonData>(
 
 
     @OptIn(ExperimentalTime::class)
-    private fun launchHealthCheck(session: DefaultClientWebSocketSession) {
+    private fun launchHealthCheck(sessionFlow: SharedFlow<Frame>) {
         var lastHealthCheckTime = Clock.System.now()
 
-        session.incoming.consumeAsFlow()
-            .filterIsInstance<Frame.Pong>()
+        sessionFlow.filterIsInstance<Frame.Pong>()
             .filter { frame -> frame.data.contentEquals(healthCheckFrame) }
             .onEach { frame -> lastHealthCheckTime = Clock.System.now() }
             .launchIn(coroutineScope)
@@ -200,31 +212,31 @@ class Crimson<SND: CrimsonData, RCV: CrimsonData>(
         }
     }
 
-    private fun respondToPingFrame(session: DefaultClientWebSocketSession){
-        session.incoming.consumeAsFlow()
-            .filterIsInstance<Frame.Ping>()
+    private fun respondToPingFrame(sessionFlow: SharedFlow<Frame>){
+        sessionFlow.filterIsInstance<Frame.Ping>()
             .mapNotNull { frame ->
                 execute(CrimsonCommand.Pong(frame.readBytes()))
             }.launchIn(coroutineScope)
     }
 
-    private fun handleIncomingFrame(session: DefaultClientWebSocketSession){
-        session.incoming.consumeAsFlow()
-            .filter {
-                (it is Frame.Text) || (it is Frame.Binary)
-            }
-            .mapNotNull { frame ->
-                when(frame) {
-                    is Frame.Text -> runCatching{ json.decodeFromString(incomingSerializer, frame.readText()) }.getOrNull()
-                    is Frame.Binary -> runCatching { json.decodeFromString(incomingSerializer, frame.readBytes().decodeToString()) }.getOrNull()
-                    else -> null
-                }
-            }
+    private fun handleIncomingFrame(sessionFlow: SharedFlow<Frame>){
+        sessionFlow.filterIsInstance<Frame.Text>()
+            .mapNotNull { frame -> runCatching{ json.decodeFromString(incomingSerializer, frame.readText()) }.getOrNull() }
             .catch { e ->
                 crimsonHandler?.onError(e)
                 execute(CrimsonCommand.Disconnect(code = 4001, "incoming frame error", abnormally = true))
             }.onEach {
-                _incomingFlow.emit(it)
+                _incomingMessageFlow.emit(it)
+            }.launchIn(coroutineScope)
+
+
+        sessionFlow.filterIsInstance<Frame.Binary>()
+            .mapNotNull { frame -> runCatching { json.decodeFromString(incomingSerializer, frame.readBytes().decodeToString()) }.getOrNull() }
+            .catch { e ->
+                crimsonHandler?.onError(e)
+                execute(CrimsonCommand.Disconnect(code = 4001, "incoming frame error", abnormally = true))
+            }.onEach {
+                _incomingMessageFlow.emit(it)
             }.launchIn(coroutineScope)
     }
 
